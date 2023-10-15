88b38b9e4a6c8e1555710e2d515f6a6d5f074d75
JavanFriedel <javanfriedel@hotmail.com>
2023-10-15 18:23:33 -0400

    Added basic functionality included generating new quotes, as well as intents to Tweet

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,59 @@
 import './App.css';
+import { useState } from 'react';
+import quotesDB from './components/quotesDB';
 
 function App() {
+  let currentQuote = 0;
+  const randomQuoteIndex = () => {
+    let newQuoteNum = Math.floor(Math.random() * quotesDB.length);
+
+    if (newQuoteNum == currentQuote) {
+      if (newQuoteNum == quotesDB.length - 1) {
+        newQuoteNum--;
+      } else {
+        newQuoteNum++;
+      }
+    }
+
+    currentQuote = newQuoteNum;
+    return newQuoteNum;
+  };
+
+  const [quote, setQuote] = useState(quotesDB[randomQuoteIndex()]);
+
+  function handleClick() {
+    setQuote(quotesDB[randomQuoteIndex()]);
+  }
+
   return (
     <>
-      <h1 className="pt-10 bg-slate-600">Initial project setep</h1>
+      <div id="outer-wrapper" className="h-screen flex flex-col justify-center">
+        <div id="quote-box" className="bg-red-100 m-auto w-2/5 p-10">
+          <h1 id="test" className="text-left m-2">
+            "{quote.quoteText}"
+          </h1>
+          <h2 id="author" className="text-right m-2">
+            - {quote.author}
+          </h2>
+          <div className="flex justify-between mt-5">
+            <a
+              id="tweet-quote"
+              className="bg-blue-400 p-2 px-4 text-slate-200"
+              href={`https://twitter.com/intent/tweet?text="${quote.quoteText}" - ${quote.author}`}
+              target="_blank"
+            >
+              Tweet
+            </a>
+            <button
+              id="new-quote"
+              onClick={handleClick}
+              className="bg-blue-400 p-2 px-4 text-slate-200"
+            >
+              New Quote
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/quotesDB.tsx
@@ -0,0 +1,144 @@
+const quotesDB = [
+  {
+    quoteText:
+      'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.',
+    author: 'Ralph Waldo Emerson',
+    date: '1830s',
+  },
+  {
+    quoteText:
+      'It is not the strongest of the species that survive, nor the most intelligent, but the one most responsive to change.',
+    author: 'Charles Darwin',
+    date: '1850s',
+  },
+  {
+    quoteText: 'The only way to do great work is to love what you do.',
+    author: 'Steve Jobs',
+    date: '2000s',
+  },
+  {
+    quoteText:
+      "In three words I can sum up everything I've learned about life: it goes on.",
+    author: 'Robert Frost',
+    date: '1920s',
+  },
+  {
+    quoteText:
+      'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+    author: 'Nelson Mandela',
+    date: '20th Century',
+  },
+  {
+    quoteText:
+      'Success is not final, failure is not fatal: It is the courage to continue that counts.',
+    author: 'Winston Churchill',
+    date: '1940s',
+  },
+  {
+    quoteText: "Life is what happens when you're busy making other plans.",
+    author: 'John Lennon',
+    date: '1970s',
+  },
+  {
+    quoteText:
+      "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+    author: 'Albert Einstein',
+    date: 'Early 20th Century',
+  },
+  {
+    quoteText:
+      'To succeed in life, you need two things: ignorance and confidence.',
+    author: 'Mark Twain',
+    date: '19th Century',
+  },
+  {
+    quoteText:
+      'The best time to plant a tree was 20 years ago. The second best time is now.',
+    author: 'Chinese Proverb',
+    date: 'Ancient China',
+  },
+  {
+    quoteText:
+      'In the end, we will remember not the words of our enemies, but the silence of our friends.',
+    author: 'Martin Luther King Jr.',
+    date: '1960s',
+  },
+  {
+    quoteText:
+      'The only thing necessary for the triumph of evil is for good men to do nothing.',
+    author: 'Edmund Burke',
+    date: '18th Century',
+  },
+  {
+    quoteText: 'Life is really simple, but we insist on making it complicated.',
+    author: 'Confucius',
+    date: 'Ancient China',
+  },
+  {
+    quoteText:
+      'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+    author: 'Nelson Mandela',
+    date: '20th Century',
+  },
+  {
+    quoteText:
+      'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.',
+    author: 'Ralph Waldo Emerson',
+    date: '1830s',
+  },
+  {
+    quoteText:
+      'It is not the strongest of the species that survive, nor the most intelligent, but the one most responsive to change.',
+    author: 'Charles Darwin',
+    date: '1850s',
+  },
+  {
+    quoteText:
+      "In three words I can sum up everything I've learned about life: it goes on.",
+    author: 'Robert Frost',
+    date: '1920s',
+  },
+  {
+    quoteText:
+      'The best time to plant a tree was 20 years ago. The second best time is now.',
+    author: 'Chinese Proverb',
+    date: 'Ancient China',
+  },
+  {
+    quoteText:
+      "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+    author: 'Albert Einstein',
+    date: 'Early 20th Century',
+  },
+  {
+    quoteText:
+      'To succeed in life, you need two things: ignorance and confidence.',
+    author: 'Mark Twain',
+    date: '19th Century',
+  },
+  {
+    quoteText:
+      'The best time to plant a tree was 20 years ago. The second best time is now.',
+    author: 'Chinese Proverb',
+    date: 'Ancient China',
+  },
+  {
+    quoteText:
+      'In the end, we will remember not the words of our enemies, but the silence of our friends.',
+    author: 'Martin Luther King Jr.',
+    date: '1960s',
+  },
+  {
+    quoteText:
+      'The only thing necessary for the triumph of evil is for good men to do nothing.',
+    author: 'Edmund Burke',
+    date: '18th Century',
+  },
+  {
+    quoteText: 'Life is really simple, but we insist on making it complicated.',
+    author: 'Confucius',
+    date: 'Ancient China',
+  },
+];
+
+export default quotesDB;
